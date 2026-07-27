04b4ba969ec3101924dd0457c6efbc74293da0fd
Sharon-rs7 <robertsharon526@gmail.com>
2026-07-27 09:44:08 +0530

    Time: 39 ms (36.88%), Space: 46.9 MB (84.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         for (int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
+                if(nums[j]==target-nums[i]){
                     return new int[]{i,j};
                 }
             }
