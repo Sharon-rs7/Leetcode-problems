b93d16faddc0db1bfb3fa7d6082693394d08cf2f
SHARON R <robertsharon526@gmail.com>
2026-08-21 15:50:09 +0530

    Time: 2 ms (11.2%), Space: 45.8 MB (22.89%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        int length=s.length();
+        StringBuilder str=new StringBuilder("");
+         char c[]=new char[length];
+        for(int i=0;i<length;i++){
+            c[indices[i]]=s.charAt(i);
+        }
+        str.append(c);
+        return str.toString();
+    }
+}
